a78091ea2c63a74b81bdde79b842dc6d518875ed
Stefan Fries <stefanefries@googlemail.com>
2015-06-01 12:37:36 +0200

    Refactored model, view and octopus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //	model
-var model = function(){
+/*var model = function(){
 	var id = 1;
 	function Cat(name, imgUrl){
 		this.name = name;
@@ -17,6 +17,28 @@ var model = function(){
 		cats.push(new Cat(names[i-1], imageURL));
 	}
 	return cats;
+}
+*/
+
+var model = {
+	id: 1,
+	Cat: function Cat(name, imgUrl){
+		this.name = name;
+		this.image = imgUrl;
+		this.counter = 0;
+		this.id = model.id++;
+	},
+	names: ['Gustav', 'Charles', 'Bjorn', 'Hans', 'Dagobert'],
+	createCats: function(names){
+		// create 5 cats
+		var cats = [];
+		for(var i=1; i < 6; i++){
+			var imageURL = 'images/cat' + i + '.jpg';
+			cats.push(new model.Cat(names[i-1], imageURL));
+		}
+		return cats;
+	}
+
 }
 
 // add cat links to DOM
@@ -53,13 +75,39 @@ var view = {
 
 			}());
 		}
-	}//(imgUrl, catname, catId, counter)
+	},
+
+	addButtonListener: function(){
+
+		$('#admin').click(function(){
+			console.log('Button clicked');
+			var admin_input = ('<div> </div>')
+			$('.input-area').append('<div >')
+		})
+	}
 }
 
 // octopus
-var octopus = function(){
+var octopus = {
+	adminmode: false,
+	init: function() {
+		var cats = model.createCats(['Gustav', 'Charles', 'Bjorn', 'Hans', 'Dagobert']);
+		view.addLinksToDom(cats);
+		view.updateDisplayArea(cats[0])();
+		view.addButtonListener();
+	}
+
+}
+
+octopus.init();
+/*
+function(){
 	var cats = model();
 	view.addLinksToDom(cats);
 	view.updateDisplayArea(cats[0])();
+	view.addButtonListener();
+
 }
+
 octopus();
+*/
